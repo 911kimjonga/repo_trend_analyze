549ec0006aa067e34225c5a9f16b5fb02cab05cb
911kimjonga <911kimjonga@gmail.com>
2025-03-27 23:02:10 +0900

    build: gradle 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,39 @@ repositories {
 }
 
 dependencies {
+    // spring boot starter
     implementation("org.springframework.boot:spring-boot-starter")
+
+    // spring boot web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // web flux
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // Serialization (KotlinX)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
+
+    // Jackson Kotlin Module
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // KotlinX + Jackson
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-jackson")
+
+    // tester
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
