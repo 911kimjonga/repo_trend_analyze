a93c459b99a722e8031acd4bfe5155dbf89ede7a
911kimjonga <911kimjonga@gmail.com>
2025-03-27 22:45:29 +0900

    init commit

---
+++ src/main/kotlin/com/repo1/demo/Repo1Application.kt
@@ -0,0 +1,11 @@
+package com.repo1.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Repo1Application
+
+fun main(args: Array<String>) {
+    runApplication<Repo1Application>(*args)
+}
---
+++ src/test/kotlin/com/repo1/demo/Repo1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.repo1.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Repo1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
