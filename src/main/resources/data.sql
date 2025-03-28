509980b077d7c449e173eeb0049f170f78c77958
김종원 <911kimjonga@polycube.co.kr>
2025-03-28 11:44:08 +0900

    feat: 기본 H2 데이터데이스 데이터 입력 SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (user_name, user_email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO users (user_name, user_email) VALUES ('Bob', 'bob.smith@example.com');
+INSERT INTO users (user_name, user_email) VALUES ('Charlie', 'charlie.brown@example.com');
+INSERT INTO users (user_name, user_email) VALUES ('David', 'david.lee@example.com');
+INSERT INTO users (user_name, user_email) VALUES ('Emma', 'emma.watson@example.com');
