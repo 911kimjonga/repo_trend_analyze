369d2c336c2ae59c1021672c1c0631265d5a072a
911kimjonga <911kimjonga@gmail.com>
2025-03-30 00:13:23 +0900

    build: data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, email) VALUES ('alice123', '1234', 'alice@example.com');
-INSERT INTO users (username, password, email) VALUES ('bob123', '1234', 'bob.smith@example.com');
-INSERT INTO users (username, password, email) VALUES ('charlie123', '1234', 'charlie.brown@example.com');
-INSERT INTO users (username, password, email) VALUES ('david123', '1234', 'david.lee@example.com');
-INSERT INTO users (username, password, email) VALUES ('emma123', '1234', 'emma.watson@example.com');
+INSERT INTO users (username, password, email) VALUES ('alice123', '$2a$10$fX2XBeVGtOSMRlFqM1qH0.dNnqRUQdpflE/I6tmUQZnIIxSzgWrlW', 'alice@example.com');
+INSERT INTO users (username, password, email) VALUES ('bob123', '$2a$10$EyfHSuuiidxP/hn713nR0.y5Mrz0GSIdqi9F7ZoaLw2cbU9PhBe3y', 'bob.smith@example.com');
+INSERT INTO users (username, password, email) VALUES ('charlie123', '$2a$10$hWpP50LA.AAWeLeWOHeMHuyPG/icoerD3SGFJ46ixs8p2sBHGcBsK', 'charlie.brown@example.com');
+INSERT INTO users (username, password, email) VALUES ('david123', '$2a$10$ED99Cs9jsS8txggk4MIoqunODyW8ZcU6YosmK/Svl3tcJ/JvA2Upy', 'david.lee@example.com');
+INSERT INTO users (username, password, email) VALUES ('emma123', '$2a$10$KRbkioGJFVzhw/QkTtqdMuaqGHywEEhJQOhNXNN8ueFRY7H2qziSu', 'emma.watson@example.com');
