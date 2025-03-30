6fa9f755eceefc223dada2bcde1f2767b04cb7af
911kimjonga <911kimjonga@gmail.com>
2025-03-30 21:28:33 +0900

    fix: Controller 수정

--- src/main/kotlin/com/repo/security/user/controller/UserController.kt
+++ src/main/kotlin/com/repo/security/user/controller/UserController.kt
@@ -1,52 +1,94 @@
 package com.repo.security.user.controller
 
-import com.repo.security.user.model.dto.SignInDto
-import com.repo.security.user.model.dto.SignUpDto
-import com.repo.security.user.model.vo.SignInVo
-import com.repo.security.user.model.vo.SignUpVo
+import com.repo.security.common.exception.CustomException
+import com.repo.security.common.exception.CustomException.*
+import com.repo.security.common.jwt.component.JwtComponent
+import com.repo.security.common.utils.ApiResponse
+import com.repo.security.user.enums.UserRole
+import com.repo.security.user.model.dto.request.SignInRequestDto
+import com.repo.security.user.model.dto.request.SignUpRequestDto
+import com.repo.security.user.model.vo.request.SignInRequestVo
+import com.repo.security.user.model.vo.request.SignUpRequestVo
+import com.repo.security.user.model.vo.response.SignInResponseVo
+import com.repo.security.user.model.vo.response.SignUpResponseVo
 import com.repo.security.user.service.UserService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user")
 class UserController(
     private val userService: UserService,
+    private val jwtComponent: JwtComponent,
 ) {
 
     @PostMapping("/signup")
     fun sign(
-        @RequestBody vo: SignUpVo
-    ): ResponseEntity<String> {
+        @RequestBody vo: SignUpRequestVo
+    ): ApiResponse<SignUpResponseVo> {
         val save = userService.saveUser(
-            SignUpDto(
+            SignUpRequestDto(
                 vo.username,
                 vo.password,
                 vo.email
             )
         )
 
-        return ResponseEntity.ok(
-            if (save) "success" else "failed"
+        return ApiResponse.ok(
+            SignUpResponseVo(
+                isSuccess = save
+            )
         )
     }
 
     @PostMapping("/signin")
     fun login(
-        @RequestBody vo: SignInVo
-    ): ResponseEntity<String> {
-        val isUser = userService.isUser(
-            SignInDto(
+        @RequestBody vo: SignInRequestVo
+    ): ApiResponse<SignInResponseVo> {
+        val user = userService.findUser(
+            SignInRequestDto(
                 vo.username,
                 vo.password
             )
         )
 
-        return ResponseEntity.ok(
-            if (isUser) "success" else "failed"
+        val token = jwtComponent.generateToken(user)
+
+        return ApiResponse.ok(
+            SignInResponseVo(
+                token = token,
+            )
+        )
+    }
+
+    @PostMapping("/check")
+    fun check(
+        @RequestHeader("Authorization") authHeader: String
+    ): ResponseEntity<String> {
+        val token = this.extractToken(authHeader)
+
+        if (jwtComponent.validateToken(token, UserRole.USER)) {
+            val id = jwtComponent.getIdFromToken(token)
+            return ResponseEntity.ok(id)
+        }
+
+        return ResponseEntity.ok("failed")
+    }
+
+    private fun extractToken(header: String): String {
+        if (!header.startsWith("Bearer ")) {
+            throw UnauthorizedException()
+        }
+        return header.removePrefix("Bearer ").trim()
+    }
+
+    @ExceptionHandler(CustomException::class)
+    fun handleCustomException(
+        e: CustomException
+    ): ApiResponse<Unit> {
+        return ApiResponse.error(
+            e.status.value().toString(),
+            e.message
         )
     }
 
