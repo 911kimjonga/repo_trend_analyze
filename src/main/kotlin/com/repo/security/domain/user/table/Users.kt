fd6405d266bbc82dd51867b34cbf74fb0ac8607e
911kimjonga <911kimjonga@gmail.com>
2025-04-02 16:42:20 +0900

    fix: Users 오브젝트 수정

--- src/main/kotlin/com/repo/security/domain/user/table/Users.kt
+++ src/main/kotlin/com/repo/security/domain/user/table/Users.kt
@@ -2,22 +2,6 @@ package com.repo.security.domain.user.table
 
 import org.jetbrains.exposed.dao.id.LongIdTable
 
-/*
-@Entity
-@Table(name = "users")
-data class User(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val userId: Long = 0,
-
-    @Column(nullable = false, length = 255)
-    val userName: String = "",
-
-    @Column(nullable = true, length = 255)
-    val userEmail: String? = null,
-)
- */
-
 object Users : LongIdTable("users") {
     val username = varchar("username", 255).uniqueIndex()
     val password = varchar("password", 255)
