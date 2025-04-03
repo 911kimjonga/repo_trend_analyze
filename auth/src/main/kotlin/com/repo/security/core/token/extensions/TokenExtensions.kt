0758252795d257d56f91e64eb5df8f2edc32f855
911kimjonga <911kimjonga@gmail.com>
2025-04-03 11:01:34 +0900

    fix: Token Provider 수정

--- auth/src/main/kotlin/com/repo/security/core/token/extensions/TokenExtensions.kt
+++ auth/src/main/kotlin/com/repo/security/core/token/extensions/TokenExtensions.kt
@@ -1,10 +1,22 @@
 package com.repo.security.core.token.extensions
 
+import com.repo.security.common.exception.SecurityException.*
+import com.repo.security.core.token.enums.AccessTokenHeaders
+import com.repo.security.core.token.enums.RefreshTokenCookies
 import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 
+fun HttpServletRequest.getAccessTokenHeader() =
+    getHeader(AccessTokenHeaders.AUTH.header)
+        ?: throw UnauthenticatedException()
+
+fun HttpServletRequest.getRefreshToken() =
+    cookies?.firstOrNull { it.name == RefreshTokenCookies.REFRESH_TOKEN.cookie }?.value
+        ?: throw UnauthenticatedException()
+
 fun HttpServletResponse.addCookieRefreshToken(refreshToken: String) {
-    val cookie = Cookie("refreshToken", refreshToken)
+    val cookie = Cookie(RefreshTokenCookies.REFRESH_TOKEN.cookie, refreshToken)
     cookie.isHttpOnly = true
     cookie.secure = true
     cookie.path = "/"
--- auth/src/main/kotlin/com/repo/security/core/token/provider/AccessTokenProvider.kt
+++ auth/src/main/kotlin/com/repo/security/core/token/provider/AccessTokenProvider.kt
@@ -3,10 +3,14 @@ package com.repo.security.core.token.provider
 import com.repo.security.common.exception.SecurityException.*
 import com.repo.security.common.exception.SecurityException.AccessTokenException.*
 import com.repo.security.core.config.JwtConfig
+import com.repo.security.core.redis.enums.KeyType
+import com.repo.security.core.redis.service.RedisService
 import com.repo.security.core.token.enums.AccessTokenClaims.*
+import com.repo.security.core.token.extensions.getAccessTokenHeader
 import com.repo.security.domain.user.enums.UserRole
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Component
 import java.time.Duration
 import java.util.*
@@ -14,6 +18,7 @@ import java.util.*
 @Component
 class AccessTokenProvider(
     private val config: JwtConfig,
+    private val redisService: RedisService,
 ) {
     private val key by lazy { Keys.hmacShaKeyFor(config.secret.toByteArray()) }
 
@@ -34,8 +39,10 @@ class AccessTokenProvider(
     }
 
     fun extractToken(
-        header: String
+        request: HttpServletRequest
     ): String {
+        val header = request.getAccessTokenHeader()
+
         if (!header.startsWith("Bearer ")) {
             throw UnauthorizedException()
         }
@@ -50,6 +57,7 @@ class AccessTokenProvider(
         val role = UserRole.fromRole(claims[ROLE.claim] as? String)
 
         when {
+            redisService.has(KeyType.BLACKLIST, token) -> throw UnauthenticatedException()
             claims.expiration.before(Date()) -> throw ExpiredAccessTokenException()
             role != expectedRole -> throw InvalidRoleException()
             else -> return true
@@ -61,12 +69,19 @@ class AccessTokenProvider(
     ): String =
         this.getClaims(token).subject
 
+    fun saveBlackList(
+        token: String
+    ) =
+        redisService.save(KeyType.BLACKLIST, token, "logout", this.getRemainingTime(token))
+
     fun getRemainingTime(
         token: String
     ): Duration =
         Duration.ofMillis(maxOf(0, this.getClaims(token).expiration.time - Date().time))
 
-    private fun getClaims(token: String) =
+    private fun getClaims(
+        token: String
+    ) =
         runCatching {
             Jwts.parser()
                 .verifyWith(key)
