83a50b2a9a8ee981c0cfda42101f91aaee0b9fe2
911kimjonga <911kimjonga@gmail.com>
2025-04-03 11:00:27 +0900

    fix: DTO 분리 및 수정

--- auth/src/main/kotlin/com/repo/security/domain/auth/model/dto/response/LoginResponseDto.kt
+++
@@ -1,9 +0,0 @@
-package com.repo.security.domain.auth.model.dto.response
-
-data class LoginResponseDto(
-    val id: String,
-    val username: String,
-    val password: String,
-    val userRole: String,
-    val status: String,
-)
