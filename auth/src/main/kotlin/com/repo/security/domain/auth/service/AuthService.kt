a4ec2af15490e69cb480d28ebed2f8f5468d6578
911kimjonga <911kimjonga@gmail.com>
2025-04-03 11:01:45 +0900

    fix: Service 수정

--- auth/src/main/kotlin/com/repo/security/domain/auth/service/AuthService.kt
+++ auth/src/main/kotlin/com/repo/security/domain/auth/service/AuthService.kt
@@ -1,42 +1,73 @@
 package com.repo.security.domain.auth.service
 
+import com.repo.security.common.exception.SecurityException.UnauthenticatedException
 import com.repo.security.core.redis.enums.KeyType
 import com.repo.security.core.redis.service.RedisService
+import com.repo.security.core.token.extensions.getRefreshToken
 import com.repo.security.core.token.provider.AccessTokenProvider
+import com.repo.security.core.token.provider.RefreshTokenProvider
+import com.repo.security.domain.auth.model.dto.request.LoginRequestDto
 import com.repo.security.domain.auth.model.dto.request.SignUpRequestDto
+import com.repo.security.domain.auth.model.dto.response.TokenResponseDto
+import com.repo.security.domain.user.enums.UserRole
+import com.repo.security.domain.user.model.dto.request.SaveRequestDto
 import com.repo.security.domain.user.service.UserService
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
-import java.time.Duration
 
 @Service
 class AuthService(
+    private val passwordEncoder: PasswordEncoder,
     private val userService: UserService,
     private val redisService: RedisService,
     private val accessTokenProvider: AccessTokenProvider,
+    private val refreshTokenProvider: RefreshTokenProvider,
 ) {
 
-    fun signUp(dto: SignUpRequestDto) =
-        userService.saveUser(dto)
+    fun signUp(
+        dto: SignUpRequestDto
+    ) =
+        userService.saveUser(
+            SaveRequestDto(
+                dto.username,
+                passwordEncoder.encode(dto.enteredPassword),
+                dto.email
+            )
+        )
 
     fun signOut() {
 
     }
 
-    fun login() {
+    fun login(
+        dto: LoginRequestDto
+    ): TokenResponseDto {
+        val user = userService.findUser(dto.username)
+        passwordEncoder.matches(dto.password, user.encryptedPassword)
 
+        return TokenResponseDto(
+            accessTokenProvider.generateAccessToken(user.id, UserRole.fromRole(user.userRole)),
+            refreshTokenProvider.generateRefreshToken(user.id)
+        )
     }
 
-    fun logout() {
+    fun logout(
+        request: HttpServletRequest,
+    ) {
+        val accessToken: String = accessTokenProvider.extractToken(request)
+        val refreshToken: String = request.getRefreshToken()
 
+        accessTokenProvider.saveBlackList(accessToken)
+        refreshTokenProvider.deleteRefreshToken(refreshToken)
     }
 
-    fun saveBlackList(token: String) {
-        val ttl = accessTokenProvider.getRemainingTime(token)
-        return redisService.save(KeyType.BLACKLIST, token, "logout", ttl)
-    }
-
-    fun isBlackList(token: String): Boolean {
-        return redisService.has(KeyType.BLACKLIST, token)
-    }
+    fun refresh(
+        refreshToken: String
+    ) =
+        TokenResponseDto(
+            refreshTokenProvider.reissueAccessToken(refreshToken),
+            refreshTokenProvider.rotateRefreshToken(refreshToken)
+        )
 
 }
--- auth/src/main/kotlin/com/repo/security/domain/user/service/UserService.kt
+++ auth/src/main/kotlin/com/repo/security/domain/user/service/UserService.kt
@@ -1,42 +1,25 @@
 package com.repo.security.domain.user.service
 
-import com.repo.security.domain.auth.model.dto.request.SignUpRequestDto
-import com.repo.security.domain.auth.model.dto.request.LoginRequestDto
-import com.repo.security.domain.auth.model.dto.response.LoginResponseDto
+import com.repo.security.domain.user.model.dto.request.SaveRequestDto
 import com.repo.security.domain.user.repository.UserRepository
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserService(
     private val repository: UserRepository,
-    private val passwordEncoder: PasswordEncoder,
 ) {
 
     @Transactional
-    fun saveUser(requestDto: SignUpRequestDto): Boolean {
-        val encryptPassword = passwordEncoder.encode(requestDto.password)
-        return repository.save(
-            SignUpRequestDto(
-                requestDto.username,
-                encryptPassword,
-                requestDto.email,
-            )
-        ).insertedCount > 0
-    }
+    fun saveUser(dto: SaveRequestDto) =
+        repository.save(dto).insertedCount > 0
 
     @Transactional
-    fun findUser(id: Long): LoginResponseDto {
-        val responseDto = repository.findById(id)
-        return responseDto
-    }
+    fun findUser(id: Long) =
+        repository.findById(id)
 
     @Transactional
-    fun findUser(requestDto: LoginRequestDto): LoginResponseDto {
-        val responseDto = repository.findByUsername(requestDto.username)
-        passwordEncoder.matches(requestDto.password, responseDto.password)
-        return responseDto
-    }
+    fun findUser(userName: String) =
+        repository.findByUsername(userName)
 
 }
