891b839e22799a78a816d2abcf856072e1db9f1f
911kimjonga <911kimjonga@gmail.com>
2025-04-03 11:00:54 +0900

    fix: User Repository 수정

--- auth/src/main/kotlin/com/repo/security/domain/user/repository/UserRepository.kt
+++ auth/src/main/kotlin/com/repo/security/domain/user/repository/UserRepository.kt
@@ -3,8 +3,8 @@ package com.repo.security.domain.user.repository
 import com.repo.security.domain.user.entity.UserEntity
 import com.repo.security.domain.user.enums.UserRole
 import com.repo.security.domain.user.enums.UserStatus
-import com.repo.security.domain.auth.model.dto.response.LoginResponseDto
-import com.repo.security.domain.auth.model.dto.request.SignUpRequestDto
+import com.repo.security.domain.user.model.dto.request.SaveRequestDto
+import com.repo.security.domain.user.model.dto.response.UserResponseDto
 import com.repo.security.domain.user.table.Users
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insertIgnore
@@ -15,26 +15,27 @@ import org.springframework.stereotype.Repository
 @Repository
 class UserRepository {
 
-    fun save(requestDto: SignUpRequestDto): InsertStatement<Long> {
+    fun save(requestDto: SaveRequestDto): InsertStatement<Long> {
         return Users.insertIgnore {
             it[username] = requestDto.username
-            it[password] = requestDto.password
+            it[password] = requestDto.encryptedPassword
             it[email] = requestDto.email
             it[userRole] = UserRole.USER.role
             it[status] = UserStatus.ACTIVE.status
         }
     }
 
-    fun findById(id: Long): LoginResponseDto {
+    fun findById(id: Long): UserResponseDto {
         val entity = UserEntity.find {
             (Users.id eq id) and
                     (Users.status eq UserStatus.ACTIVE.status)
         }.singleOrNull() ?: throw IllegalArgumentException()
 
-        return LoginResponseDto(
+        return UserResponseDto(
             entity.id.toString(),
             entity.username,
             entity.password,
+            entity.email,
             entity.userRole,
             entity.status
         )
@@ -46,16 +47,17 @@ class UserRepository {
             .count()
     }
 
-    fun findByUsername(username: String): LoginResponseDto {
+    fun findByUsername(username: String): UserResponseDto {
         val entity = UserEntity.find {
             (Users.username eq username) and
                     (Users.status eq UserStatus.ACTIVE.status)
         }.singleOrNull() ?: throw IllegalArgumentException()
 
-        return LoginResponseDto(
+        return UserResponseDto(
             entity.id.toString(),
             entity.username,
             entity.password,
+            entity.email,
             entity.userRole,
             entity.status
         )
