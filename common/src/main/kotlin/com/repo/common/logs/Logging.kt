2d1932fb6c198d15749431e3e2e4725bf1524cab
911kimjonga <911kimjonga@gmail.com>
2025-04-03 16:56:50 +0900

    fix: 공통 로깅 메소드 수정

--- common/src/main/kotlin/com/repo/common/logs/Logging.kt
+++ common/src/main/kotlin/com/repo/common/logs/Logging.kt
@@ -5,16 +5,16 @@ import org.slf4j.LoggerFactory
 
 inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
 
-inline fun <reified T> T.logInfo(message: String) {
-    logger().info(message)
+inline fun <reified T> T.logInfo(message: String, vararg params: Any) {
+    logger().info(message, params)
 }
 
-inline fun <reified T> T.logDebug(message: String) {
-    logger().debug(message)
+inline fun <reified T> T.logDebug(message: String, vararg params: Any) {
+    logger().debug(message, params)
 }
 
-inline fun <reified T> T.logWarn(message: String) {
-    logger().warn(message)
+inline fun <reified T> T.logWarn(message: String, vararg params: Any) {
+    logger().warn(message, params)
 }
 
 inline fun <reified T> T.logError(message: String, throwable: Throwable? = null) {
