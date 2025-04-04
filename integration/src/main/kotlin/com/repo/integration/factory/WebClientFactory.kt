9883d8c238d0c2ce9e07e05c225eae7db8f0bead
911kimjonga <911kimjonga@gmail.com>
2025-04-04 09:33:41 +0900

    feat: WebClientFactory 구현

--- integration/src/main/kotlin/com/repo/integration/factory/WebClientFactory.kt
+++ integration/src/main/kotlin/com/repo/integration/factory/WebClientFactory.kt
@@ -1,15 +1,19 @@
 package com.repo.integration.factory
 
 import com.repo.integration.enums.Protocols
+import io.netty.handler.ssl.SslContextBuilder
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import reactor.netty.http.client.HttpClient
+import java.io.FileInputStream
+import java.security.KeyStore
 import java.time.Duration
 import java.util.concurrent.TimeUnit
-import io.netty.handler.timeout.ReadTimeoutHandler
-import io.netty.handler.timeout.WriteTimeoutHandler
-import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
 
 
 object WebClientFactory {
@@ -21,10 +25,10 @@ object WebClientFactory {
         connectionTimeout: Int = 5000,
         readTimeout: Int = 3000,
         protocol: Protocols = Protocols.TLS_1_2,
-        trustManagers: Array<TrustManager>? = null,
+        trustManagerFactory: TrustManagerFactory = InsecureTrustManagerFactory.INSTANCE
     ): WebClient {
 
-        val httpClient = HttpClient.create()
+        var httpClient = HttpClient.create()
             .option(io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS, connectionTimeout)
             .responseTimeout(Duration.ofMillis(readTimeout.toLong()))
             .doOnConnected { conn ->
@@ -32,14 +36,38 @@ object WebClientFactory {
                 conn.addHandlerLast(WriteTimeoutHandler(readTimeout.toLong(), TimeUnit.MILLISECONDS))
             }
 
+        if (useSSL) {
+            val sslContext = SslContextBuilder.forClient()
+                .trustManager(trustManagerFactory)
+                .protocols(protocol.protocol)
+                .build()
+
+            httpClient = httpClient.secure { spec ->
+                spec.sslContext(sslContext)
+            }
+        }
+
         return WebClient.builder()
             .clientConnector(ReactorClientHttpConnector(httpClient))
             .defaultHeaders { headers ->
                 headers.accept = listOf(MediaType.APPLICATION_JSON, MediaType.valueOf("text/json"))
                 headers.contentType = MediaType.APPLICATION_JSON
             }
-            .defaultUriVariables(emptyMap<String, Any>())
             .build()
+    }
+
+    fun createTrustManagerFactory(
+        trustStorePath: String,
+        trustStorePassword: String,
+        trustStoreType: String = "JKS" // 또는 "PKCS12"
+    ): TrustManagerFactory {
+        val keyStore = KeyStore.getInstance(trustStoreType)
+        keyStore.load(FileInputStream(trustStorePath), trustStorePassword.toCharArray())
+
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(keyStore)
+
+        return trustManagerFactory
     }
 
 }
