7d07b15b2bc8563e950d6786e15dd15dc0fea3d4
911kimjonga <911kimjonga@gmail.com>
2025-04-05 20:35:45 +0900

    refactor: Auth 모듈 Redis 관련 리팩토링

--- auth/src/main/kotlin/com/repo/auth/core/redis/service/AuthRedisService.kt
+++ auth/src/main/kotlin/com/repo/auth/core/redis/service/AuthRedisService.kt
@@ -1,28 +1,28 @@
 package com.repo.auth.core.redis.service
 
 import com.repo.auth.common.exception.AuthException.RefreshTokenException.InvalidRefreshTokenException
-import com.repo.auth.core.redis.enums.KeyType
+import com.repo.auth.core.redis.enums.AuthRedisKeyType
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import java.time.Duration
 
 @Service
-class RedisService(
+class AuthRedisService(
     @Qualifier("authRedisTemplate") private val redisTemplate: StringRedisTemplate
 ) {
 
-    fun save(keyType: KeyType, token: String, userId: String, expiry: Duration) =
+    fun save(keyType: AuthRedisKeyType, token: String, userId: String, expiry: Duration) =
         redisTemplate.opsForValue().set("${keyType.type}:$token", userId, expiry)
 
-    fun delete(keyType: KeyType, token: String) =
+    fun delete(keyType: AuthRedisKeyType, token: String) =
         redisTemplate.delete("${keyType.type}:$token")
 
-    fun get(keyType: KeyType, token: String): String =
+    fun get(keyType: AuthRedisKeyType, token: String): String =
         redisTemplate.opsForValue().get("${keyType.type}:$token")
             ?: throw InvalidRefreshTokenException()
 
-    fun has(keyType: KeyType, token: String): Boolean =
+    fun has(keyType: AuthRedisKeyType, token: String): Boolean =
         redisTemplate.hasKey("${keyType.type}:$token")
 
 }
